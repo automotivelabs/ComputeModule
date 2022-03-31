8ac223475e8c1db3eb7e458f02ffb254e4da4929
François-Xavier Kim <74252080+frkim@users.noreply.github.com>
2022-03-31 09:24:54 +0000

    fix

--- src/Converters/ConvertUtils/ConvertUtils.cs
+++ src/Converters/ConvertUtils/ConvertUtils.cs
@@ -33,12 +33,12 @@ namespace ConvertUtils
             return (value - 32) / 1.8;
         }
         
-        public double ConvertToDegree(double rad)
+        public  static double ConvertToDegree(double rad)
         {
             return (180 / Math.PI) * rad;
         }
         
-        public double ConvertToRadians(double angle)
+        public static double ConvertToRadians(double angle)
         {
             return (System.Math.PI / 180) * angle;
         }
